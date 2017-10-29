e5b2c75346b8daadcfb683b8cef8700587cce1c0
GWD <gaoweidong127@163.com>
2017-10-29 12:24:14 +0800

    初始化工程

---
+++ src/main/java/com/nwpu/emall/EmallWapApplication.java
@@ -0,0 +1,12 @@
+package com.nwpu.emall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmallWapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmallWapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nwpu/emall/EmallWapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nwpu.emall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmallWapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
